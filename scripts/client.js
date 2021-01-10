217eade03c8d83d0938d70eba18386c1db7544b0
foggy <>
2021-01-10 15:18:24 -0600

    Complete

--- scripts/client.js
+++ scripts/client.js
@@ -3,51 +3,45 @@ let tSalary = 0;
 //functions
 
 function onReady(){
-  //append test data fields. if added can only add 1 new
-  /*$('#empTbl tbody').append(`
-    <tr>
-      <td>FirstName</td>
-      <td>LastName</td>
-      <td>ID</td>
-      <td>Title</td>
-      <td>AnnualSalary</td>
-      <td><button id='dButton'>Delete</button></td>
-    </tr>
-  `);*/
-
-  $('#sButton').on('click', function(event){
-    let fName = $('#inputFName').val();
+ $('#sButton').on('click', function(event){//when the submit button is clicked
+    let fName = $('#inputFName').val();//setting variables catch form field
     let lName = $('#inputLName').val();
     let iNumber = $('#inputIdNumber').val();
     let iTitle = $('#inputTitle').val();
     let aSalary = $('#inputAnSalary').val();
-      //$('#empTbl tbody').empty();
-      $('#empTbl tbody').append(`
+  //append in above variables from fields to body of table
+  $('#empTbl tbody').append(`
         <tr>
           <td>${fName}</td>
           <td>${lName}</td>
           <td>${iNumber}</td>
           <td>${iTitle}</td>
-          <td>${aSalary}</td>
+          <td class='sal'>${aSalary}</td>
           <td><button class='dButton'>Delete</button></td>
         </tr>
       `);
-    tSalary += Number(aSalary) / 12;
-   
+    tSalary += Number(aSalary) / 12;//equation to calculate accumlated salary
+  updateMonthlyTotal();//run update function from line 36
+   $('.dButton').off('click');//used to stop proagation of delete button click
+   $('.dButton').on('click', function(event){//targets the nearest parent with id for removal from table
+    const thingThatWasClickedOn = $(event.target);//setting needed variables
+    let aSalary = thingThatWasClickedOn.closest('tr').find('.sal').text();
+    let newvar = Number(aSalary) / 12;//converts string to number and evaluates to monthly
+    tSalary -= newvar;//equation to calculate decreased salary
+    thingThatWasClickedOn.closest('tr').remove();
+  updateMonthlyTotal();//run update function from line 36
+    })
+});
+}//end onReady function
+function updateMonthlyTotal(){//empties and appends the salary to the DOM with css modifier
   $('#cash').empty();
-  $('#cash').append(`${tSalary}`);
+  $('#cash').append(`${tSalary.toFixed(2)}`);//adds salary and sets it to 2 decimal points
   if (tSalary > 20000){
     $('#tMonthly').addClass('redBack');
-  }    
-  });
-
-
-  $('#empTbl tbody').on('click', function(event){//not targeting right
-    const thingThatWasClickedOn = $(event.target);
-    thingThatWasClickedOn.closest('tr').remove();
-    
-  });
-}//end firstfunction
-
+  }
+  else {
+    $('#tMonthly').removeClass('redBack');
+  }
+}
 //end functions
-console.log('Connected');
+console.log('Testing');
